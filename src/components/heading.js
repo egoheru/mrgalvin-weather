0d1fbaf04e38e5421853889ba46746327aba61a1
egoheru <egoherokugit@gmail.com>
2021-01-10 09:34:32 -0800

    updated

---
+++ src/components/heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = (props) => {
+    return (
+        <div>
+            <h1>What,s the Weather like</h1>
+            <h3>Find out what the weather condition in your city.</h3>
+        </div>
+    )
+}
+
+export default Heading;
